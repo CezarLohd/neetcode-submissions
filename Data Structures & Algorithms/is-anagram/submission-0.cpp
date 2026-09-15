0208389322cbfbae352f168bff90b8bd8e17d197
CezarLohd <115188160+CezarLohd@users.noreply.github.com>
2026-09-15 20:49:37 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> s_chars;
+        unordered_map<char,int> t_chars;
+
+        for (char& ch : s) {
+            if (!s_chars.contains(ch)) 
+                s_chars.insert({ch, 1});
+            else
+                ++s_chars[ch];
+        }
+
+        for (char& ch : t) {
+            if (!t_chars.contains(ch)) 
+                t_chars.insert({ch, 1});
+            else
+                ++t_chars[ch];
+        }
+
+        return s_chars == t_chars ? true : false;
+    }
+};

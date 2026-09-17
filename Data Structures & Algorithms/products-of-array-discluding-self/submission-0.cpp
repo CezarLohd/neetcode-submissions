b48becef7e04db5788befa7d12ba1645758c868d
CezarLohd <115188160+CezarLohd@users.noreply.github.com>
2026-09-17 11:29:07 +0200

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> solution;
+        solution.reserve(nums.size());
+        int product = 1;
+        int zeros = 0;
+
+        for (int i : nums)
+            if (i == 0) ++zeros;
+            else product *= i;
+        
+        for (int i : nums)
+            if (zeros > 1) 
+                solution.push_back(0);
+            else if (zeros == 1)
+                solution.push_back(i == 0 ? product : 0);
+            else
+                solution.push_back(product / i);
+        
+        return solution;
+    }
+};

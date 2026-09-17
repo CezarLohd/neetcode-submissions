5ee80ba71f8d91aebb7c72ed4ba97a6b5063b030
CezarLohd <115188160+CezarLohd@users.noreply.github.com>
2026-09-17 12:00:19 +0200

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<int> rows[9];
+        unordered_set<int> columns[9];
+        unordered_set<int> squares[3][3];
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char now = board[i][j];
+                if (now == '.') continue;
+                
+                if (rows[i].contains(now)) return false;
+                if (columns[j].contains(now)) return false;
+                if (squares[i/3][j/3].contains(now)) return false;
+
+                rows[i].insert(now);
+                columns[j].insert(now);
+                squares[i/3][j/3].insert(now);
+            }
+        }
+
+        return true;
+    }
+};

5bfced6793b419e6f6a91ef3b2d28bee51000b93
CezarLohd <115188160+CezarLohd@users.noreply.github.com>
2026-09-17 13:11:59 +0200

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s.empty()) return true;
+        // lepsze bo w locie sprawdzam isalnum zamiast to na chama na poczatku erasowac (tez przesuwac w pamieci, nawet jak powiedzmy pierwszy i ostatni juz sa inne)
+
+        for (auto left = s.begin(), right = s.end() - 1; left < right; ++left, --right) {
+            while (left < right && !isalnum(*left)) ++left;
+            while (left < right && !isalnum(*right)) --right;
+
+            if (tolower(*left) != tolower(*right)) return false;
+        }
+        
+        return true;
+    }
+};
